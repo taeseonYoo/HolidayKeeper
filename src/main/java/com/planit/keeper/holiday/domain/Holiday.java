f240ddb1aa5494d832c4c1e5cdb4fbe150e9d313
taeseonYoo <jkl0642@hansung.ac.kr>
2025-12-03 22:55:05 +0900

    feat: holiday 엔티티 인덱스 추가
    - holiday_year 필드 단일 인덱스
    - country_code 필드 단일 인덱스
    - holiday_year, country_code 복합 인덱스

--- src/main/java/com/planit/keeper/holiday/domain/Holiday.java
+++ src/main/java/com/planit/keeper/holiday/domain/Holiday.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,13 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(
+        indexes = {
+                @Index(name = "idx_holiday_year",columnList = "holiday_year"),
+                @Index(name = "idx_country_code",columnList = "country_code"),
+                @Index(name = "idx_holiday_year_country_code",columnList = "holiday_year,country_code")
+        }
+)
 public class Holiday {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
